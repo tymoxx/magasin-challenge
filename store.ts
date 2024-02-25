999334a6675798df8c809e7e5571560841718dc9
Yuri <7777kara@gmail.com>
2024-02-25 14:58:10 +0100

    persist data

--- store.ts
+++ store.ts
@@ -1,14 +1,33 @@
-import { configureStore } from '@reduxjs/toolkit'
-import {launchesApi} from "@/services/launchesApi";
-import {favouritesReducer} from "@/features/favourites/favouritesSlice";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { configureStore } from '@reduxjs/toolkit';
+import { launchesApi } from "@/services/launchesApi";
+import { favouritesReducer } from "@/features/favourites/favouritesSlice";
+import { combineReducers } from 'redux';
+import { persistReducer, persistStore } from 'redux-persist';
 
-export const store = configureStore({
-  reducer: {
+const reducers = combineReducers({
     favourites: favouritesReducer,
     [launchesApi.reducerPath]: launchesApi.reducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware().concat(launchesApi.middleware)})
+});
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: ['persist/PERSIST'],
+            },
+        }).concat(launchesApi.middleware),
+});
+
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
